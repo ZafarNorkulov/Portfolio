908d537da63a2153fe674c2b53f37cae173a0909
ZafarNorkulov <norqulovzafar123@gmail.com>
2023-12-15 11:26:59 +0500

    responsive done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,76 +1,70 @@
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import {  bwmap, worldmap } from '../assets';
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { bwmap, worldmap } from "../assets";
 
 const Hero = () => {
   return (
     <>
       <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-        <img
-          src={bwmap}
-          alt="world map"
-          className="w-full h-full md:block hidden object-cover"
-        />
+        <img src={bwmap} alt="world map" className="w-full h-full md:block hidden object-cover" />
       </div>
       <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-        <img
-          src={worldmap}
-          alt="world map"
-          className="w-full h-full md:hidden block object-cover"
-        />
+        <img src={worldmap} alt="world map" className="w-full h-full md:hidden block object-cover" />
       </div>
       <section
         className="relative flex md:flex-row flex-col w-full h-screen mx-auto 
-        md:bg-hero bg-hero-mobile overflow-hidden">
+        md:bg-hero bg-hero-mobile overflow-hidden"
+      >
         <div
-          className={`absolute inset-0 top-[150px] 
-          lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
+          className={`absolute inset-0 top-[120px] 
+          lg:top-[150px] xl:top-[200px] ${styles.paddingX} 
           max-w-7xl mx-auto flex flex-row items-start
-          justify-between gap-3`}>
+          justify-between gap-3`}
+        >
           <div className="flex flex-col justify-center items-center mt-5 ml-3">
             <div className="w-5 h-5 rounded-full bg-[#0a0a0a] md:hidden" />
             <div className="w-1 md:h-80 h-40 bw-gradient md:hidden" />
           </div>
 
           <div>
-            <h1
-              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I&apos;m{' '}
+            <h1 className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
+              Hi, I&apos;m{" "}
               <span
                 className="md:text-battleGray md:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
-                font-extrabold uppercase">
-               Zafar Norkulov
+                font-extrabold uppercase"
+              >
+                Zafar Norkulov
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-            <span className='whitespace-nowrap'>I&apos;m a Frontend Developer.</span> <br className="md:block hidden" />
-             Welcome to my portfolio site!
+              <span className="whitespace-nowrap">I&apos;m a Frontend Developer.</span>{" "}
+              <br className="md:block hidden" />
+              Welcome to my portfolio site!
             </p>
           </div>
           <div
             className="w-screen flex flex-col items-start 
-            justify-center md:-ml-[3rem] xxs:mt-4"></div>
+            justify-center md:-ml-[3rem] xxs:mt-4"
+          ></div>
 
           <div></div>
         </div>
 
         <div
           className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
+          flex justify-center items-center"
+        >
           <a href="#about">
-            <div
-              className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
-            justify-center items-start p-2">
+            <div className=" xl:w-[35px] xl:h-[64px] md:w-[30px] md:h-[56px] w-[24px] h-[44px] rounded-3xl border-4 border-french border-dim md:flex hidden justify-center items-start xl:p-2 md:p-[6px] p-[4px]">
               <motion.div
                 animate={{
-                  y: [0, 24, 0],
+                  y: [0, 20, 0],
                 }}
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType: 'loop',
+                  repeatType: "loop",
                 }}
                 className="w-3 h-3 rounded-full bg-taupe mb-1"
               />
@@ -79,9 +73,7 @@ const Hero = () => {
         </div>
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
-        <div>
-         
-        </div>
+        <div></div>
       </section>
     </>
   );
